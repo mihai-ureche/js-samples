5392d6387805acdffa311890764171e13c641ff5
Mihai Ureche <mihai.ureche@gmail.com>
2023-12-27 11:54:53 +0000

    show points on map

--- index.ts
+++ index.ts
@@ -4,33 +4,52 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+
+// import points
+import points from './points.json';
+
+
 // Initialize and add the map
 let map;
 async function initMap(): Promise<void> {
   // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  const position = { lat: 47.492764, lng: 27.3213593 };
 
   // Request needed libraries.
   //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+  const { Map, Polyline } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
 
   // The map, centered at Uluru
   map = new Map(
     document.getElementById('map') as HTMLElement,
     {
-      zoom: 4,
+      zoom: 10,
       center: position,
       mapId: 'DEMO_MAP_ID',
     }
   );
 
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: 'Uluru'
+
+
+
+  // create path for this
+
+  const path = points.map(point => {
+    return {
+      lng: point[0], lat: point[1]
+    }
   });
+
+  const polyline = new Polyline({
+    path, 
+    strokeColor: "red",
+    map
+  })
+
+
+
+
 }
 
 initMap();
